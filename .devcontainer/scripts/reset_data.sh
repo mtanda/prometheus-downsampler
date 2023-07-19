e4f97f6f428bc14c1675a1200ba1e449da8a7a58
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2023-07-19 18:27:36 +0900

    loading test data by restart

--- .devcontainer/scripts/reset_data.sh
+++ .devcontainer/scripts/reset_data.sh
@@ -9,3 +9,7 @@ done
 rm -f /tmp/metrics.dat
 go run gen_test_data.go > /tmp/metrics.dat
 sudo sudo -u nobody promtool tsdb create-blocks-from openmetrics /tmp/metrics.dat /prometheus
+curl -X PUT 'http://prometheus:9090/-/quit' && echo
+while [ `curl 'http://prometheus:9090/-/ready' -s -o /dev/null -w '%{http_code}\n'` != 200 ]; do
+  sleep 1
+done
